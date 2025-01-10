da066d2c16c3f51e77f12153ee227c1270415393
Ed Page <eopage@gmail.com>
2025-01-10 09:36:33 -0600

    test(error): Show lack of origin

--- tests/testsuite/errors.rs
+++ tests/testsuite/errors.rs
@@ -112,6 +112,28 @@ fn test_get_invalid_type() {
 
 #[test]
 #[cfg(feature = "json")]
+fn test_get_invalid_type_file() {
+    let c = Config::builder()
+        .add_source(File::new(
+            "tests/testsuite/get-invalid-type.json",
+            FileFormat::Json,
+        ))
+        .build()
+        .unwrap();
+
+    let res = c.get::<bool>("boolean_s_parse");
+
+    assert!(res.is_err());
+    assert_data_eq!(
+        res.unwrap_err().to_string(),
+        str![[
+            r#"invalid type: string "fals", expected a boolean for key `boolean_s_parse` in tests/testsuite/get-invalid-type.json"#
+        ]]
+    );
+}
+
+#[test]
+#[cfg(feature = "json")]
 fn test_get_missing_field() {
     #[derive(Debug, Deserialize)]
     struct InnerSettings {
@@ -128,6 +150,32 @@ fn test_get_missing_field() {
     "inner": { "value": 42 }
 }
         "#,
+            FileFormat::Json,
+        ))
+        .build()
+        .unwrap();
+
+    let res = c.get::<InnerSettings>("inner");
+    assert_data_eq!(
+        res.unwrap_err().to_string(),
+        str!["missing field `value2` for key `inner`"]
+    );
+}
+
+#[test]
+#[cfg(feature = "json")]
+fn test_get_missing_field_file() {
+    #[derive(Debug, Deserialize)]
+    struct InnerSettings {
+        #[allow(dead_code)]
+        value: u32,
+        #[allow(dead_code)]
+        value2: u32,
+    }
+
+    let c = Config::builder()
+        .add_source(File::new(
+            "tests/testsuite/get-missing-field.json",
             FileFormat::Json,
         ))
         .build()
@@ -317,6 +365,47 @@ fn test_deserialize_invalid_type() {
 
 #[test]
 #[cfg(feature = "json")]
+fn test_deserialize_invalid_type_file() {
+    #[derive(Deserialize, Debug)]
+    struct Place {
+        #[allow(dead_code)]
+        name: usize, // is actually s string
+    }
+
+    #[derive(Deserialize, Debug)]
+    struct Output {
+        #[allow(dead_code)]
+        place: Place,
+    }
+
+    let c = Config::builder()
+        .add_source(File::new(
+            "tests/testsuite/deserialize-invalid-type.json",
+            FileFormat::Json,
+        ))
+        .build()
+        .unwrap();
+
+    let res = c.try_deserialize::<Output>();
+    let e = res.unwrap_err();
+    assert_data_eq!(
+        e.to_string(),
+        str![[
+            r#"invalid type: string "Torre di Pisa", expected an integer for key `place.name` in tests/testsuite/deserialize-invalid-type.json"#
+        ]]
+    );
+    if let ConfigError::Type {
+        key: Some(path), ..
+    } = e
+    {
+        assert_eq!(path, "place.name");
+    } else {
+        panic!("Wrong error {:?}", e);
+    }
+}
+
+#[test]
+#[cfg(feature = "json")]
 fn test_deserialize_missing_field() {
     #[derive(Debug, Deserialize)]
     struct Settings {
@@ -339,6 +428,38 @@ fn test_deserialize_missing_field() {
     "inner": { "value": 42 }
 }
         "#,
+            FileFormat::Json,
+        ))
+        .build()
+        .unwrap();
+
+    let res = c.try_deserialize::<Settings>();
+    assert_data_eq!(
+        res.unwrap_err().to_string(),
+        str!["missing field `value2` for key `inner`"]
+    );
+}
+
+#[test]
+#[cfg(feature = "json")]
+fn test_deserialize_missing_field_file() {
+    #[derive(Debug, Deserialize)]
+    struct Settings {
+        #[allow(dead_code)]
+        inner: InnerSettings,
+    }
+
+    #[derive(Debug, Deserialize)]
+    struct InnerSettings {
+        #[allow(dead_code)]
+        value: u32,
+        #[allow(dead_code)]
+        value2: u32,
+    }
+
+    let c = Config::builder()
+        .add_source(File::new(
+            "tests/testsuite/deserialize-missing-field.json",
             FileFormat::Json,
         ))
         .build()
