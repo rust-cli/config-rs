a3d68ce55e8acef7b57e7156cf41d1dc68b1630a
Ed Page <eopage@gmail.com>
2025-07-08 15:43:46 -0500

    refactor(toml): Pull out table conversion

--- src/file/format/toml.rs
+++ src/file/format/toml.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 
-use crate::format;
 use crate::map::Map;
 use crate::value::Value;
 
@@ -9,8 +8,18 @@ pub(crate) fn parse(
     text: &str,
 ) -> Result<Map<String, Value>, Box<dyn Error + Send + Sync>> {
     // Parse a TOML value from the provided text
-    let value = from_toml_value(uri, &toml::from_str(text)?);
-    format::extract_root_table(uri, value)
+    let table = from_toml_table(uri, &toml::from_str(text)?);
+    Ok(table)
+}
+
+fn from_toml_table(uri: Option<&String>, table: &toml::Table) -> Map<String, Value> {
+    let mut m = Map::new();
+
+    for (key, value) in table {
+        m.insert(key.clone(), from_toml_value(uri, value));
+    }
+
+    m
 }
 
 fn from_toml_value(uri: Option<&String>, value: &toml::Value) -> Value {
@@ -21,12 +30,7 @@ fn from_toml_value(uri: Option<&String>, value: &toml::Value) -> Value {
         toml::Value::Boolean(value) => Value::new(uri, value),
 
         toml::Value::Table(ref table) => {
-            let mut m = Map::new();
-
-            for (key, value) in table {
-                m.insert(key.clone(), from_toml_value(uri, value));
-            }
-
+            let m = from_toml_table(uri, table);
             Value::new(uri, m)
         }
 
